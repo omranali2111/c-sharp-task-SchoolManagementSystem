c52ff561eda97af7a951780e5cbba03e1448097a
omranali2111 <aljabri1234@hotmail.com>
2023-07-02 12:55:18 +0400

    SchoolManagementSystem

---
+++ c-sharp-task-SchoolManagementSystem/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_sharp_task_SchoolManagementSystem
+{
+    
+    internal class Person
+    {
+        string Name;
+        int Age;
+        string Address;
+
+        public Person(string Name,int Age,string Address) 
+        { 
+            this.Name = Name;
+            this.Age = Age;
+            this.Address = Address;
+        }
+
+        public void displayPerson() 
+        {
+            Console.WriteLine($"name is  {Name} Age is {Age} Address is {Address}");
+        }
+    }
+}
---
+++ c-sharp-task-SchoolManagementSystem/Program.cs
@@ -0,0 +1,9 @@
+﻿using c_sharp_task_SchoolManagementSystem;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+      Person person=new Person("omran",27,"seeb");
+    }
+}
